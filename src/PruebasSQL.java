8d8812b617ef509ea3439c85eccf109c1168ce5b
germangithub <german@asymmetrystudio.com>
2015-11-05 21:47:18 -0300

    Insercion del jdbc

--- src/PruebasSQL.java
+++ src/PruebasSQL.java
@@ -6,7 +6,7 @@ public class PruebasSQL {
 	
 	public static void main(String[] args) {
 			
-		conexion.getNombres();
+		
 		
 	}
 
--- src/sql/Conexion.java
+++ src/sql/Conexion.java
@@ -10,26 +10,20 @@ public class Conexion {
 	final static String user = "asymmetr_web";
 	final static String password = "53491221";
 	final static String base = "asymmetr_pruebas";
-	Connection con;
+	static Connection con;
 		
 	public Conexion(){
-		
+				
 		try {
 		
-			Connection con = DriverManager.getConnection( host , user , password );
+			con = DriverManager.getConnection( host , user , password );
 		
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
 		
 		}
-				
-	}
-	
-	public void getNombres(){
 		
-		System.out.println(con);
-	
 	}
-
+		
 }
